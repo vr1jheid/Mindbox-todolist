0b852a317f1316a4e0a01e503b0629650458fc23
vr1jheid <vrijheid.rromg@gmail.com>
2024-08-15 18:23:35 +0300

    feat: add todo input

---
+++ src/Components/Todo/TodoInput/TodoInput.tsx
@@ -0,0 +1,37 @@
+import { Button, TextInput } from "@mantine/core";
+import { memo, useContext, useState } from "react";
+import { TodoContext } from "../Context/TodoContext";
+
+export const TodoInput = memo(() => {
+  const [input, setInput] = useState("");
+  const { addTodo } = useContext(TodoContext);
+
+  const submitTodo = () => {
+    if (!input.length) return;
+    addTodo(input);
+    setInput("");
+  };
+
+  return (
+    <form
+      style={{ display: "flex", gap: 7 }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        submitTodo();
+      }}
+    >
+      <TextInput
+        value={input}
+        onKeyDown={({ key }) => {
+          if (key !== "Enter") return;
+          submitTodo();
+        }}
+        placeholder="Whats need to be done?"
+        onChange={({ target }) => setInput(target.value)}
+      />
+      <Button variant="filled" color="cyan" type="submit">
+        Add
+      </Button>
+    </form>
+  );
+});
